cbd21cfcac3a533a44953288267759e75bf308d9
Tomoya148665 <>
2023-08-23 16:05:30 +0000

    Updates

---
+++ _next/static/_oRqr0x9xAx2kqn4PsqEn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-19da7407d9a987af.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
